f32b04d7b19726f2668e825c7d1cd83836af5f82
eamonhinton <neontechnologyimports@gmail.com>
2024-08-24 20:34:09 -0400

    efficiency and new example call
    got runtime down to below 152 microseconds per bond

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 fn bond_pricer(face: f32, coup_rate: f32, disc_rate: f32, years: f32, freq: f32) -> f32 {
-    let mut price:f32 = 0.0;
     let coup_rate = coup_rate / freq;
     let new_discr: f32 = 1.0 + (disc_rate / freq);
     let periods_to_maturity = years * freq;
-    for payment_num in 1..=periods_to_maturity as i32{
-        price += (face * coup_rate) / new_discr.powf(payment_num as f32);
+    let discount_factor = new_discr.recip(); // Equivalent to 1.0 / new_discr
+
+    let mut price: f32 = 0.0;
+    let mut discounted_cash_flow: f32 = 1.0;
+
+    for _ in 1..=periods_to_maturity as i32 {
+        discounted_cash_flow *= discount_factor; // Accumulate the power of the discount factor
+        price += (face * coup_rate) * discounted_cash_flow;
     }
-    price += face / new_discr.powf(periods_to_maturity);
-    return price;
+    
+    price += face * discounted_cash_flow;
+    price
 }
 
 fn main() {
-    bond_pricer(37603.0 , 0.05, 0.01,3.0, 4.0);
+    bond_pricer(100.0 , 0.03875, 0.03, 10.0, 2.0);
 }
